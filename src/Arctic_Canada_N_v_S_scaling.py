766a408d326aae6b428d9010f73a20276a71fb79
Jason Box <jason@Jasons-MacBook-Pro.local>
2023-09-29 11:23:22 +0200

    improvements

--- src/Arctic_Canada_N_v_S_scaling.py
+++ src/Arctic_Canada_N_v_S_scaling.py
@@ -3,19 +3,30 @@
 """
 Created on Tue Mar 21 09:09:10 2023
 
+Note:
+    mb_annual_hydrological obtained from /Users/jason/Dropbox/GRACE/Wouters/GRACE_Arctic_Wouters/GRACE_Arctic.py
+    but has issues related to my attempts to in-fill gaps...
+    A more robust solution to the goal of obtaining a 2002-2015 average mass change is to fit to multi-annual monthly results using:
+        /Users/jason/Dropbox/GRACE/Wouters/GRACE_Arctic_Wouters/comp_Wouters_GRACE.py
+
 @author: jason
+
+Notes:
+    RegionnS(km2)V(km3)
+    Arctic Canada North 3,205    105,139    34,399+/-4,699
+    Arctic Canada South 6,67940,893 9,814+/-1,115
+    
+    ACN_volume=34,399+/-4,699
+    ACS_volume=9,814+/-1,115
+
 """
 
 import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 import os
 import pandas as pd
 from datetime import datetime 
 from numpy.polynomial.polynomial import polyfit
-
 
 if os.getlogin() == 'jason':
     base_path = '/Users/jason/Dropbox/GRACE/Wouters/GRACE_Arctic_Wouters'
@@ -35,6 +46,19 @@ N=np.nanmean(ACN.mass_balance_sept_to_sept_Gt[v])
 print(N)
 
 N_S_ratio=N/S
+print(N/S)
+
+fig, ax = plt.subplots(figsize=(12,12))
+
+plt.plot(ACN.year[v],ACN.mass_balance_sept_to_sept_Gt[v],label='ACN mass_balance_sept_to_sept_Gt')
+plt.plot(ACS.year[v],ACS.mass_balance_sept_to_sept_Gt[v],label='ACS mass_balance_sept_to_sept_Gt')
+plt.legend()
+
+#%%
+N_S_ratio=1.0654 # obtained from Wouters 2023 GLAMBIE data using:
+    # /Users/jason/Dropbox/GRACE/Wouters/GRACE_Arctic_Wouters/GRACE_Arctic.py
+    # /Users/jason/Dropbox/GRACE/Wouters/GRACE_Arctic_Wouters/comp_Wouters_GRACE.py
+
 print(N/S)
 
 #%%
